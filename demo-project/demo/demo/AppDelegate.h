85f093cb0a56269bf2140ca899ac698ebafe5106
Helmut Januschka <h.januschka@krone.at>
2016-09-30 11:37:27 +0200

    dmeo project

---
+++ demo-project/demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Januschka Helmut on 30.09.16.
+//  Copyright © 2016 hjanuschka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo-project/demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by Januschka Helmut on 30.09.16.
+//  Copyright © 2016 hjanuschka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
