22f7f278efe2703ce7f70221ed4afb7958b59fc2
Helmut Januschka <h.januschka@krone.at>
2017-01-24 14:32:23 +0100

    fix tests

--- spec/update_project_codesigning_action_spec.rb
+++ spec/update_project_codesigning_action_spec.rb
@@ -1,7 +1,7 @@
 describe Fastlane::Actions::UpdateProjectCodesigningAction do
   describe '#run' do
     it 'Updates a Project to manual signing' do
-      expect(Fastlane::UI).to receive(:success).with("Successfully updated project settings to use ProvisioningStyle 'Manual'")
+      expect(Fastlane::UI).to receive(:success).with("Modified Targets:")
 
       Fastlane::Actions::UpdateProjectCodesigningAction.run(path: "demo-project/demo/demo.xcodeproj")
     end
